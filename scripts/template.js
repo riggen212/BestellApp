a2d324d6272fbab646dc82d5207fd602b24a7c43
riggen212 <bastiriggen212@gmail.com>
2026-05-10 16:50:32 +0200

    menu card template added styling for menu card added menu categories added on landingpage JSON for menus created

---
+++ scripts/template.js
@@ -0,0 +1,18 @@
+function renderMenuCard() {
+    return `<div class="menuCard">
+                <article class="menuContentCard">
+                    <figure>
+                        <img src="./assets/img/veggie_mushroom_black-burger.jpg" alt="">
+                    </figure>
+                    <div class="menuDescription">
+                        <h3>Veggie mushroom black burger</h3>
+                        <p>Mixed green salad, Tomatoes, Edamame, Mushrooms</p>
+                    </div>
+                    <div class="menuPrice">
+                        <span>16,90€</span>
+                        <button>Add to basket</button>
+                    </div>
+                </article>
+            </div>
+    `;
+}
