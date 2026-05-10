070913826d16fb528476544d4006f44126743a8c
riggen212 <bastiriggen212@gmail.com>
2026-05-10 21:32:42 +0200

    footer and styling added template for all categories adjusted logic for all menu categories implemented

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,82 @@
 const KEYS = Object.keys(menus);
-let categoryName = [];
 
 function showMenus() {
     for (let i = 0; i < menus.length; i++) {
-        categoryName = menus[KEYS[i]];
 
-        renderBurgerMenus(categoryName, i);
+        if (i == 0) {
+            renderBurgerMenus(0);
+        } else if (i == 1) {
+            renderPizzaMenus(1);
+        } else if (i == 2) { 
+            renderSaladMenus(2);
+        } else {
+            return;
+        }
     };
 }
 
-function renderBurgerMenus(category, i) {
-    if (category = menus[KEYS[i]].burger) {
-        let arrayLength = menus[KEYS[i]].burger.length;
-        let elementId = document.getElementById('burgerMenu');
+function renderBurgerMenus(i) {
+    let arrayLength = menus[KEYS[0]].burger.length;
+    let elementId = document.getElementById('burgerMenu');
 
-        for (let cardNumber = 0; cardNumber < arrayLength; cardNumber++) {
-            console.log(cardNumber);
-            elementId.innerHTML += renderMenuCard(cardNumber);
-            getBurgerData(cardNumber);
-        }
+    for (let cardNumber = 0; cardNumber < arrayLength; cardNumber++) {
+        elementId.innerHTML += renderMenuCard(cardNumber, 0);
+        getBurgerData(cardNumber);
     }
 }
 
-function getBurgerData(index) {    
-    let name = document.getElementById(`name${index}`);
-    let description = document.getElementById(`description${index}`);
-    let price = document.getElementById(`price${index}`);
+function getBurgerData(index) {
+    let name = document.getElementById(`name0${index}`);
+    let description = document.getElementById(`description0${index}`);
+    let price = document.getElementById(`price0${index}`);
+    let imagePath = document.getElementById(`imagePath0${index}`)
 
-    name.innerHTML += menus[KEYS[0]].burger[index].menuName;   
+    name.innerHTML += menus[KEYS[0]].burger[index].menuName;
     description.innerHTML += menus[KEYS[0]].burger[index].menuDescription;
     price.innerHTML += menus[KEYS[0]].burger[index].menuPrice.toFixed(2) + '€';
+    imagePath.setAttribute("src", `./assets/img/${menus[KEYS[0]].burger[index].menuImageName}`);
+}
+
+function renderPizzaMenus(i) {
+        let arrayLength = menus[KEYS[1]].pizza.length;
+        let elementId = document.getElementById('pizzaMenu');
+
+        for (let cardNumber = 0; cardNumber < arrayLength; cardNumber++) {
+            elementId.innerHTML += renderMenuCard(cardNumber, 1);
+            getPizzaData(cardNumber);
+        }
+}
+
+function getPizzaData(index) {
+    let name = document.getElementById(`name1${index}`);
+    let description = document.getElementById(`description1${index}`);
+    let price = document.getElementById(`price1${index}`);
+    let imagePath = document.getElementById(`imagePath1${index}`)
+
+    name.innerHTML += menus[KEYS[1]].pizza[index].menuName;
+    description.innerHTML += menus[KEYS[1]].pizza[index].menuDescription;
+    price.innerHTML += menus[KEYS[1]].pizza[index].menuPrice.toFixed(2) + '€';
+    imagePath.setAttribute("src", `./assets/img/${menus[KEYS[1]].pizza[index].menuImageName}`);
+}
+
+function renderSaladMenus(i) {
+        let arrayLength = menus[KEYS[1]].pizza.length;
+        let elementId = document.getElementById('saladMenu');
+
+        for (let cardNumber = 0; cardNumber < arrayLength; cardNumber++) {
+            elementId.innerHTML += renderMenuCard(cardNumber, 2);
+            getSaladData(cardNumber);
+        }
+}
+
+function getSaladData(index) {
+    let name = document.getElementById(`name2${index}`);
+    let description = document.getElementById(`description2${index}`);
+    let price = document.getElementById(`price2${index}`);
+    let imagePath = document.getElementById(`imagePath2${index}`)
+
+    name.innerHTML += menus[KEYS[2]].salad[index].menuName;
+    description.innerHTML += menus[KEYS[2]].salad[index].menuDescription;
+    price.innerHTML += menus[KEYS[2]].salad[index].menuPrice.toFixed(2) + '€';
+    imagePath.setAttribute("src", `./assets/img/${menus[KEYS[2]].salad[index].menuImageName}`);
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,15 +1,16 @@
-function renderMenuCard(index) {
+function renderMenuCard(index, numb) {
     return `<div class="menuCard">
                 <article class="menuContentCard">
-                    <img id="imagePath${index}" src="./assets/img/veggie_mushroom_black-burger.jpg" alt="">
-
+                    <figure>
+                        <img id="imagePath${numb}${index}" src="" alt="">
+                    </figure>
                     <div class="menu">
                         <div class="menuDescription">
-                            <h3 id="name${index}"></h3>
-                            <p id="description${index}"></p>
+                            <h3 id="name${numb}${index}"></h3>
+                            <p id="description${numb}${index}"></p>
                         </div>
                         <div class="menuPrice">
-                            <span id="price${index}"></span>
+                            <span id="price${numb}${index}"></span>
                             <button>Add to basket</button>
                         </div>
                     </div>
